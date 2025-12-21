ac0a68e1ef8207f176ce7eaa64ebd66872d5fc9a
2021Cris <90016651+2021Cris@users.noreply.github.com>
2025-12-20 23:43:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/**
- * Configuración optimizada para despliegues en Vercel.
- */
+// Configuración optimizada para Vercel y el proyecto deepfix-os1
 export default defineConfig({
   plugins: [react()],
   base: '/',
