54ab73b2df28a033db50e8bb546e010441fe2081
2021Cris <90016651+2021Cris@users.noreply.github.com>
2025-12-23 20:56:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración básica para el proyecto Vite + React
 export default defineConfig({
   plugins: [react()],
 })
