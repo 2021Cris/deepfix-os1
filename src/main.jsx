ec7d403025499026790786b643355056ec9c518f
2021Cris <90016651+2021Cris@users.noreply.github.com>
2025-12-23 21:23:09 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Este archivo conecta el componente principal App con el div 'root' del index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
